e741a4ea61a2aa321ba219b55ac7d00e498d323f
RAJ RAUTTRAY <jasminbibiworkweb45@gmail.com>
2025-07-17 18:02:26 +0530

    XQUEEN

--- plugins/restart.js
+++ plugins/restart.js
@@ -3,7 +3,7 @@ const { sleep } = require("../lib/functions");
 
 cmd({  
     pattern: "restart",  
-    desc: "Restart CRISS-AI",  
+    desc: "Restart BINORI-ROBOT",  
     category: "owner",  
     filename: __filename  
 },  
